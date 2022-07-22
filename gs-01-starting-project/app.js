a96abec613e466f8f821917729cbe45221588a68
Devin Knight <devin.knight@accenture.com>
2022-07-22 14:25:51 -0400

    first commit

---
+++ gs-01-starting-project/app.js
@@ -0,0 +1,17 @@
+// =====  This is vanilla javascript to adjust the page. =====
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
+
+
+// function addGoal () {
+//     const enteredValue = inputEl.value;
+//     const listItemEl = document.createElement('li');
+
+//     listItemEl.textContent = enteredValue;
+//     listEl.appendChild(listItemEl);
+//     inputEl.value = '';
+// }
+
+
+// buttonEl.addEventListener('click', addGoal);
