c28ff35823e3a34ae26df5361d17b7ffadaf779d
Devin Knight <devin.knight@accenture.com>
2022-07-22 14:45:12 -0400

    Creating my first global Vue object with associated logic to mount to / update the page.

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,3 +1,22 @@
+
+// Here we call the global Vue object. (takes a JS object to configure our Vue app)
+Vue.createApp({
+    data: function() {
+        return {
+            goals: [],
+            enteredValue: ''
+        }
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app'); // our .mount method tells Vue where in our HTML to use our configuration above.
+
+
+// ===========================================================
 // =====  This is vanilla javascript to adjust the page. =====
 // const buttonEl = document.querySelector('button');
 // const inputEl = document.querySelector('input');
