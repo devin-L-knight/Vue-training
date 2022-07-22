7411fbf3d18fc4af4b813aef706b96717f80f073
Devin Knight <devin.knight@accenture.com>
2022-07-22 16:26:29 -0400

    created the Vue object with the config for the data & methods. Updated the HTML to accomodate changes in app.js

---
+++ vue_project/app.js
@@ -0,0 +1,24 @@
+// create the Vue instance of the app & pass a JS object as a config
+const app = Vue.createApp({
+    data: function () {
+        return {
+            name: 'Devin',
+            age: 36,
+            imageURL: 'https://miro.medium.com/fit/c/176/176/1*gNh9gUcCesdGK_k9TIH6Yw.png'
+        };
+    },
+    methods: {
+        ageInFive: function(age) {
+            return this.age + 5;
+        },
+        getAge: function() {
+            return this.age;
+        },
+        randomNumber: function() {
+            // will generate a random number between 1 and 0
+            return Math.random().toFixed(2);
+        }
+    }
+})
+
+app.mount('#assignment');
