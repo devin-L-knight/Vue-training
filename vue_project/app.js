ecce8f703f7dc9075d6f0494b8d419e4c84830cd
Devin Knight <devin.knight@accenture.com>
2022-07-25 10:30:29 -0400

    reset code to the starter code of the beginning of the section.

--- vue_project/app.js
+++ vue_project/app.js
@@ -1,25 +1,10 @@
-// use the Globally available Vue object to create the app.
-// we pass it a JS object, which serves as our configuration
 const app = Vue.createApp({
-    data: function() {
-        return {
-            courseGoalA: 'Finish Vue.js Course',
-            courseGoalB: 'Master Vue & build slick apps!',
-            vueLink: 'https://vuejs.org/'
-        };
+    data() {
+      return {
+        counter: 0,
+      };
     },
-    methods: {
-        outputGoal: function() {
-            const randomNumber = Math.random();
-            if (randomNumber < 0.5) {
-                return this.courseGoalA
-            } else {
-                return this.courseGoalB
-            }
-        }
-    }
-})
-
-
-// Here we'll mount our Vue app to the correct part of our HTML.
-app.mount('#user-goal');
+  });
+  
+  app.mount('#events');
+  
