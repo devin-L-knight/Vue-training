e8c38ba1c532aac3aa043a484fc7ede651d754f7
Devin Knight <devin.knight@accenture.com>
2022-07-25 13:42:35 -0400

    Added a lastName data property & input...and created a counter watcher to reset the count when the value is above or below a certain point (NOTE: this is best type of scenario to use a watcher, otherwise a computed property or method may be best option).

--- vue_project/app.js
+++ vue_project/app.js
@@ -3,28 +3,34 @@ const app = Vue.createApp({
       return {
         counter: 0,
         name: '',
-        fullName: ''
+        lastName: ''
+        // fullName: ''
         // confirmedName: ''
       };
     },
     watch: {
-        // when the name data property changes...this watcher will be executed automatically by Vue.js when the property changes
-        name(value) {
-            if (value === '') {
-                console.log('Empty Input...')
-            } else {
-                this.fullName = value + ' ' + 'Knight';
-                console.log('NAME WAS CHANGED...')
+        // // when the name data property changes...this watcher will be executed automatically by Vue.js when the property changes
+        // name(value) {
+        //     if (value === '') {
+        //         console.log('Empty Input...')
+        //     } else {
+        //         this.fullName = value + ' ' + 'Knight';
+        //         console.log('NAME WAS CHANGED...')
+        //     }
+        // }
+        counter: function(value) {
+            if (value > 50 || value < -80) {
+                this.counter = 0;
             }
         }
     },
     computed: {
         fullName: function() {
             console.log('RUNNING AGAIN...') // (using this computed property in place of the outputFullName method...so it's only called when we want instead of called every time something changes on the screen).
-            if (this.name === '') {
+            if (this.name === '' || this.lastName === '') {
                 return '';
             } else {
-                return this.name + ' ' + 'Knight';
+                return this.name + ' ' + this.lastName;
             }
         }
     },
