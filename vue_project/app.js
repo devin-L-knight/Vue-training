6ab10962e48f1938c1167e2fe51a2bc6167922a0
Devin Knight <devin.knight@accenture.com>
2022-07-25 14:57:25 -0400

    refactored watcher to watch for message change & cleaned up the conditionals within the computed property.

--- vue_project/app.js
+++ vue_project/app.js
@@ -6,33 +6,27 @@ const app = Vue.createApp({
         }
     },
     watch: {
-        result(value) {
+        message(value) {
+            // whenever message has a new value...we'll trigger this watcher
             // console.log('Timer started');
-            // setTimeout((value) => {
-            //     console.log('result is >>>>>> : ', value)
-            //     value = 0;
-            // }, 5000)
-            if (value > 0) {
-                console.log('TIMER HAS STARTED...')
-                setTimeout(() => {
-                    this.result = 0;
-                    console.log('result reset to zero');
-                }, 5000)
-            }
+            console.log('value >>> ', value);
+            const that = this
+            setTimeout(() => {
+                that.result = 0;
+                that.message = `Game Reset...Result is ${that.result}`
+            }, 5000)
         }
     },
     computed: {
         displayMessage: function() {
             // console.log('displayMessage ran...');
 
-            if (this.result !== 0 && this.result < 37) {
+            if (this.result < 37) {
                 return this.message = 'Not there yet';
             } else if (this.result > 37) {
                 return this.message = 'Too much!';
-            } else if (this.result === 37) {
-                return this.message = `Nailed it! You got ${this.result}`;
             } else {
-                return this.message = '';
+                return this.message = `Nailed it! You got ${this.result}`;
             }
         }
     },
