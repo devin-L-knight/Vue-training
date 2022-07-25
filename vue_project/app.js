8d94ba0f18dfed781351200ed28c2b15bc17dbea
Devin Knight <devin.knight@accenture.com>
2022-07-25 14:07:47 -0400

    Created methods to add to the value of the result and implemented shorthand syntax to call the functions onClick.

--- vue_project/app.js
+++ vue_project/app.js
@@ -3,6 +3,16 @@ const app = Vue.createApp({
         return {
             result: 0
         }
+    },
+    methods: {
+        addFive: function() {
+            console.log('add five has run...')
+            this.result = this.result + 5;
+        },
+        addOne: function() {
+            console.log('add one has run....')
+            this.result++
+        }
     }
 });
 
