8a4c30f814106396db56f14298bddf0acc1e587c
Devin Knight <devin.knight@accenture.com>
2022-07-26 11:18:28 -0400

    removed console logs to clean-up code.

--- vue_project/app.js
+++ vue_project/app.js
@@ -9,12 +9,8 @@ const app = Vue.createApp({
     },
     methods: {
         addTask() {
-            // console.log('entered task ? ', this.enteredTask);
             this.tasks.push(this.enteredTask);
             this.enteredTask = '';  // clear the input AFTER the task has been added
-
-            // console.log('ARRAY ??? ', this.tasks);
-            // console.log('is array? ', Array.isArray(this.tasks))
         },
         removeTask(index) {
             this.tasks.splice(index, 1); // remove the task from the list at the given index
