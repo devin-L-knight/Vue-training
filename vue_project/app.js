27eef08fb8f8b5cd268d2730eb8fbd3e83753fa0
Devin Knight <devin.knight@accenture.com>
2022-07-26 10:50:06 -0400

    added v-model to bind the enteredTask data property & checked if task is available in addTask method.

--- vue_project/app.js
+++ vue_project/app.js
@@ -2,11 +2,12 @@ const app = Vue.createApp({
     data() {
         return {
             tasks: [],
+            enteredTask: ''
         }
     },
     methods: {
         addTask() {
-
+            console.log('entered task ? ', this.enteredTask);
         }
     }
 })
