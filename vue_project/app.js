23af9e1efd2e4ec1722b97a107cd507753fe6116
Devin Knight <devin.knight@accenture.com>
2022-07-26 12:16:06 -0400

    created attackMonster & attackPlayer methods as well as a standard JS function to create a random number for our damage.

--- vue_project/app.js
+++ vue_project/app.js
@@ -1,8 +1,25 @@
+// non-Vue controlled functions / properties (standard javaScript)
+function getRandomDamage(min, max) {
+    // create a random number between a max & minimum value (will become our damage)
+    return Math.floor(Math.random() * (max - min))
+}
+
 const app = vue.createApp({
     data() {
         return {
             monsterHealth: 100,
             playerHealth: 100
+        }
+    },
+    methods: {
+        attackMonster() {
+            const monsterDamage = getRandomDamage(17, 5); // create a random number between 5 - 17
+            this.monsterHealth = this.monsterHealth - monsterDamage;
+            this.attackPlayer(); // attack the player by default after the monster is attacked
+        },
+        attackPlayer() {
+            const playerDamage = getRandomDamage(21, 9); // create a random number between 9 - 21
+            this.playerHealth = this.playerHealth - playerDamage;
         }
     }
 });
