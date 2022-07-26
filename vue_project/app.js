1441b6ad7ff5be410284cb50dd46902c870f5234
Devin Knight <devin.knight@accenture.com>
2022-07-26 10:55:08 -0400

    cleared the input after task has been added to the data.tasks array

--- vue_project/app.js
+++ vue_project/app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
         addTask() {
             // console.log('entered task ? ', this.enteredTask);
             this.tasks.push(this.enteredTask);
+            this.enteredTask = '';  // clear the input AFTER the task has been added
 
             // console.log('ARRAY ??? ', this.tasks);
             // console.log('is array? ', Array.isArray(this.tasks))
