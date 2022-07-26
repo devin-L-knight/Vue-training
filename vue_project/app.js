9f71c2a98a6683e4ab173c5ac75f1ab6ab451439
Devin Knight <devin.knight@accenture.com>
2022-07-26 16:01:24 -0400

    creating a new vue app with a separate id and using the template property to update the HTML with a string version of an element.

--- vue_project/app.js
+++ vue_project/app.js
@@ -17,3 +17,47 @@ const app = Vue.createApp({
   
   app.mount('#app');
   
+
+// creating another Vue app with a separate id located in our HTML. 
+  const app2 = Vue.createApp({
+    template: `
+        <p>{{ favoriteMeal }}</p>
+    `,
+    data() {
+        return {
+            favoriteMeal: 'steak'
+        }
+    },
+    methods: {
+
+    }
+  });
+
+  app2.mount('#app2');
+
+
+
+// // how proxies work...
+// const data = {
+//     message: 'Hello!',
+//     longMessage: 'Hello! World!'
+// };
+
+// // contains certain "Traps" the Proxy instance expects to use
+// const handler = {
+//     set(target, key, value) {
+//         console.log(target);
+//         console.log(key);
+//         console.log(value);
+//         if (key === 'message') {
+//             target.longMessage = value + ' World!';
+//         }
+//         target.message = value;
+//     }
+// };
+
+// const proxy = new Proxy(data, handler);
+
+// proxy.message = 'Hello!!!!!!';
+
+// console.log(proxy.longMessage);
