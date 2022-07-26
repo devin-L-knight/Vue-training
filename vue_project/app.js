fe665969b81ce4547c288ed2207f777ea829fc1a
Devin Knight <devin.knight@accenture.com>
2022-07-26 17:52:49 -0400

    added lifestyle hooks with consoles for context of how they operate in our basic app (NOTE: open browser dev tools & set breakpoints in lifecycles to observe changes in the DOM).

--- vue_project/app.js
+++ vue_project/app.js
@@ -14,8 +14,44 @@ const app = Vue.createApp({
         removeGoal(idx) {
             this.goals.splice(idx, 1); // remove the element from the array at the given index
         }
+    },
+      // lifecycle hooks
+      beforeCreate() {
+        console.log('beforeCreate()');
+    },
+    created() {
+        console.log('created()');
+    },
+    beforeMount() {
+        // this triggers right before something is triggered in the screen
+        console.log('beforeMount()');
+    },
+    mounted() {
+        // at this point...the vue app was initialized internally and is output to the screen
+        console.log('mounted()');
+    },
+    beforeUpdate() {
+        // runs before our DOM is updated (before the update is fully processed)
+        console.log('beforeUpdate()');
+    },
+    updated() {
+        // runs AFTER our DOM has been updated (after the update has been processed)
+        console.log('updated()');
+    },
+    beforeUnmount() {
+        // the last chance to work with the app before it is unmounted
+        console.log('beforeUnmount()');
+    },
+    unmounted() {
+        // the app has bee unmounted by this time and is removed.
+        console.log('unmounted()');
     }
   });
   
   app.mount('#user-goals');
   
+
+  setTimeout(() => {
+    // this is rarely used, but is a way to unmount an app
+    app.unmount('#user-goals');
+  }, 3000)
