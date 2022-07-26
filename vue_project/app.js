ec73abf0fe628f20e8e96f84a3e150a87d02ac04
Devin Knight <devin.knight@accenture.com>
2022-07-26 12:27:04 -0400

    used computed property to return an object that is used in our dynamic style on the markup (used in the :style).

--- vue_project/app.js
+++ vue_project/app.js
@@ -11,6 +11,14 @@ const app = Vue.createApp({
             playerHealth: 100
         }
     },
+    computed: {
+        monsterBarStyles() {
+            return { width: this.monsterHealth + '%' }
+        },
+        playerBarStyles() {
+            return { width: this.playerHealth + '%' }
+        }
+    },
     methods: {
         attackMonster() {
             const monsterDamage = getRandomDamage(5, 16); // create a random number between 5 - 17            
