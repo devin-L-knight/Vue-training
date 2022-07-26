542f7d8ad72c3e88e6f74816c3c20af36cb71048
Devin Knight <devin.knight@accenture.com>
2022-07-26 12:23:35 -0400

    added dynamic styles & @click in our markup to adjust player / monster health on attack, adjusted attack values in app.js.

--- vue_project/app.js
+++ vue_project/app.js
@@ -4,7 +4,7 @@ function getRandomDamage(min, max) {
     return Math.floor(Math.random() * (max - min))
 }
 
-const app = vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
             monsterHealth: 100,
@@ -13,12 +13,12 @@ const app = vue.createApp({
     },
     methods: {
         attackMonster() {
-            const monsterDamage = getRandomDamage(17, 5); // create a random number between 5 - 17
+            const monsterDamage = getRandomDamage(5, 16); // create a random number between 5 - 17            
             this.monsterHealth = this.monsterHealth - monsterDamage;
             this.attackPlayer(); // attack the player by default after the monster is attacked
         },
         attackPlayer() {
-            const playerDamage = getRandomDamage(21, 9); // create a random number between 9 - 21
+            const playerDamage = getRandomDamage(9, 19); // create a random number between 9 - 21
             this.playerHealth = this.playerHealth - playerDamage;
         }
     }
