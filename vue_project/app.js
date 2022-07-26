f3928a2d7fd78d27e8ee3b538fc57ea7326085c1
Devin Knight <devin.knight@accenture.com>
2022-07-26 18:04:31 -0400

    reset code to starter and created vueApp to output some data to a new html template.

--- vue_project/app.js
+++ vue_project/app.js
@@ -1,58 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            goals: [],
-            enteredGoal: '',
-        };
-    },
-    methods: {
-        addNewGoal() {
-            this.goals.push(this.enteredGoal);
-
-            console.log('GOALS >>>> ', this.enteredGoal);
-        },
-        removeGoal(idx) {
-            this.goals.splice(idx, 1); // remove the element from the array at the given index
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '310-555-5555',
+                    email: 'manuel@localemail.com'
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '818-543-2211',
+                    email: 'julie@localhost.com'
+                }
+            ]
         }
-    },
-      // lifecycle hooks
-      beforeCreate() {
-        console.log('beforeCreate()');
-    },
-    created() {
-        console.log('created()');
-    },
-    beforeMount() {
-        // this triggers right before something is triggered in the screen
-        console.log('beforeMount()');
-    },
-    mounted() {
-        // at this point...the vue app was initialized internally and is output to the screen
-        console.log('mounted()');
-    },
-    beforeUpdate() {
-        // runs before our DOM is updated (before the update is fully processed)
-        console.log('beforeUpdate()');
-    },
-    updated() {
-        // runs AFTER our DOM has been updated (after the update has been processed)
-        console.log('updated()');
-    },
-    beforeUnmount() {
-        // the last chance to work with the app before it is unmounted
-        console.log('beforeUnmount()');
-    },
-    unmounted() {
-        // the app has bee unmounted by this time and is removed.
-        console.log('unmounted()');
     }
-  });
-  
-  app.mount('#user-goals');
-  
+});
 
-  // SET TIMEOUT IS TO CHECK THE UNMOUNT METHOD. (DON'T FORGET TO REMOVE / COMMENT OUT WHEN REVIEWING APP).
-  setTimeout(() => {
-    // this is rarely used, but is a way to unmount an app
-    app.unmount('#user-goals');
-  }, 3000)
+app.mount('#app')
